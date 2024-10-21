3301fa74c4a9b48f6b618a77de74c6a26cc3a1d4
carrington-115 <frumarkcarrington@gmail.com>
2024-10-21 17:25:31 +0530

    learned how to structure an object in python and how to organise the object.

--- automate-the-boring-stuff-notes/try-oop-file.py
+++ automate-the-boring-stuff-notes/try-oop-file.py
@@ -1,19 +1,22 @@
 class Student:
-    ...
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Invalid name")
+        self.name = name
+        self.house = house
 
+    def __str__(self):
+        return "This is a student"
 
 def main():
     student = get_student_info()
-    print(f"{student.name} from {student.house}")
+    # print(f"{student.name} from {student.house}")
+    print(student)
 
 def get_student_info():
-    student = Student()
-    student.name = input("Name: ")
-    student.house = input("House: ")
-    return student # this method is just by returning using a tuple but by another way
-
-
-
-
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house) # this method is just by returning using a tuple but by another way
+   
 if __name__ == "__main__":
     main()
