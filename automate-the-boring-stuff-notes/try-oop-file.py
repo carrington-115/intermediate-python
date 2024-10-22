b9460eb43ca5d2f6c2afd9663d6ccff444b2837b
carrington-115 <frumarkcarrington@gmail.com>
2024-10-22 08:40:11 +0530

    started learning how to write class custom methods

--- automate-the-boring-stuff-notes/try-oop-file.py
+++ automate-the-boring-stuff-notes/try-oop-file.py
@@ -1,22 +1,24 @@
 class Student:
-    def __init__(self, name, house):
-        if not name:
+    def __init__(self, first_name, last_name, home_address):
+        if not first_name or not last_name:
             raise ValueError("Invalid name")
-        self.name = name
-        self.house = house
+        self.first_name = first_name
+        self.last_name = last_name
+        self.home_address = home_address
 
     def __str__(self):
-        return "This is a student"
+        return f"This is {self.first_name} from {self.home_address}"
 
 def main():
     student = get_student_info()
-    # print(f"{student.name} from {student.house}")
     print(student)
+    print(f"{student.first_name} {student.last_name} from {student.home_address}")
 
 def get_student_info():
-    name = input("Name: ")
-    house = input("House: ")
-    return Student(name, house) # this method is just by returning using a tuple but by another way
-   
+    first_name = input("Frist name: ")
+    last_name = input("Last name: ")
+    home_address = input("address: ")
+    return Student(first_name, last_name, home_address) # this method is just by returning using a tuple but by another way
+
 if __name__ == "__main__":
     main()
