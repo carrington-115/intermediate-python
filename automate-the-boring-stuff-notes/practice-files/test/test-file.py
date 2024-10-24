8ea79a51100868d7176fe4e7b812b039dd2554d5
carrington-115 <frumarkcarrington@gmail.com>
2024-10-24 13:11:27 +0530

    added the test file

--- automate-the-boring-stuff-notes/practice-files/test/test-file.py
+++ automate-the-boring-stuff-notes/practice-files/test/test-file.py
@@ -3,9 +3,17 @@ from calc import Calculator
 
 new_calculator = Calculator(1, 3)
 
+def add(x, y):
+    return x + y
+
 def add_test():
-    result = new_calculator.addition()
+    result = add(2, 2)
     assert result == 4
+
+def divide_test():
+    with pytest.raises(ZeroDivisionError):
+        result = new_calculator.divide(4, 2)
+        assert result == 2
 
 add_test()
 def main():
