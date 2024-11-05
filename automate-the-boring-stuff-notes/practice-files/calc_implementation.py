92ab8944272be023c2d8a05721bc4e95262b118c
carrington-115 <frumarkcarrington@gmail.com>
2024-11-05 07:46:28 +0530

    wrote first script to handle ValueError and NameError in python

--- automate-the-boring-stuff-notes/practice-files/calc_implementation.py
+++ automate-the-boring-stuff-notes/practice-files/calc_implementation.py
@@ -1,5 +1,10 @@
 def main():
     print('hello world')
+    try:
+        number = int(input("\nWhat is the number?\n"))
+        print(f"The number is {number}")
+    except ValueError:
+        print("The value you entered is not an integer")
 
 if __name__ == "__main__":
     main()
